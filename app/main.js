61bbfc2c64a659b7dc7c0e69c77d103681ef3959
notlelouch <notlelouchvb@gmail.com>
2023-12-26 17:13:39 +0530

    testing1

--- app/main.js
+++ app/main.js
@@ -4,7 +4,9 @@ console.log("Logs from your program will appear here!");
 
 const server = net.createServer((connection) => {
   // Handle connection
-  console.log("+PONG\r\n")
+  connection.on('data', (data) => {
+    connection.write('+PONG\r\n');
+  });
 });
 
 server.listen(6379, "127.0.0.1");
