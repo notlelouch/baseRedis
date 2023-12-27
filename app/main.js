f68969a7449cbd4a2e7de7b0f1b96636d977e723
notlelouch <notlelouchvb@gmail.com>
2023-12-27 22:48:46 +0530

    implemented response for ECHO command

--- app/main.js
+++ app/main.js
@@ -6,8 +6,22 @@ server.on('connection', (connection) => {
   console.log("New client connected");
   connection.on('data', (data) => {
     const commands = data.toString().split("\r\n")
+    
     commands.filter(command => command === "ping").forEach(() => {
       connection.write("+PONG\r\n");
+    });
+    commands.forEach((command, index) => {
+      
+      if (command.toLocaleUpperCase === "PING") {
+        connection.write("+PONG\r\n");
+      }
+
+      if (command.toLocaleUpperCase === "ECHO") {
+        let msg = commands[index + 2]
+        if (msg) {
+          connection.write(`+${msg}\r\n`);
+        }
+      }
     });
   });
   // Ending connection
