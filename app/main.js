e298e9ded9455779551336977299155373e9398d
notlelouch <notlelouchvb@gmail.com>
2023-12-27 15:53:14 +0530

    handling concurrent connections

--- app/main.js
+++ app/main.js
@@ -1,13 +1,19 @@
 const net = require("net");
 
-const server = net.createServer((connection) => {
-  // Handle connection
+const server = net.createServer();
+// Handling concurrent connections
+server.on('connection', (connection) => {
+  console.log("New client connected");
   connection.on('data', (data) => {
     const commands = data.toString().split("\r\n")
-    commands.filter(command => command === "ping").array.forEach(() => {
+    commands.filter(command => command === "ping").forEach(() => {
       connection.write("+PONG\r\n");
     });
   });
-});
+  // Ending connection
+  connection.on('end', () => {
+    console.log("Client disconnected");
+  });
+}); 
 
 server.listen(6379, "127.0.0.1");
